560b1e627329dfb4270781c64a126321ccb675ba
liu.yalei <liu.yalei@mwee.cn>
2020-01-09 15:15:21 +0800

    add login js and less

---
+++ src/layouts/Login.js
@@ -0,0 +1,72 @@
+import React, {Component} from "react";
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+require('./login.less');
+
+
+class Login extends React.Component {
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div className='web-login'>
+                <div className='web-login-top'>
+                    <div className='header'>
+                        <a href="https://github.com/kuhami/web-admin" target={'_blank'}><span>WebAdmin</span></a>
+                    </div>
+                    <div className='description'>开箱即用的中台前端/设计解决方案。</div>
+                </div>
+                <div className='web-login-content'>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('username', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    size="large"
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    size="large"
+                                    placeholder="Password"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(<Checkbox>Remember me</Checkbox>)}
+                            <a className="login-form-forgot" href="">
+                                Forgot password
+                            </a>
+                            <Button size="large" type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+        );
+    }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(Login);
+
+export default  WrappedNormalLoginForm
