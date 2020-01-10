14d69f1ba9fc98039bd4d78168339608423f5746
liu.yalei <liu.yalei@mwee.cn>
2020-01-10 16:20:28 +0800

    生产 webpack 配置

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,74 @@
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.css$/,
+                // exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    use:'css-loader',
+                    fallback:'style-loader'
+                })
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use:ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    }],
+                    fallback: "style-loader"
+                }),
+            },
+            {
+                test: /\.(bmp|gif|jpeg|jpg|png)$/,
+                exclude: /node_modules/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'img/[name]_[hash:8].[ext]',
+                },
+            },
+        ]
+    },
     plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
+        new ExtractTextPlugin({
+            filename:'styles.css',
+            allChunks:true,
+            disable: process.env.NODE_ENV === "development"
+        }),
+        new HtmlWebpackPlugin({
+            title: '管理输出',
+            filename: 'index.html',
+            template: 'index.html',
         }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
-    ]
+    ],
+    devServer: {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 9000,
+        openPage: '#/home',
+        stats: 'errors-only',
+        proxy: {
+            "/api": {
+                target: "http://192.168.33.30:84",
+                pathRewrite: {"^/api" : ""}
+            }
+        }
+    }
 });
