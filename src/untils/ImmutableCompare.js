85bed109e83128e69641fdd00150bba1ce802137
liu.yalei <liu.yalei@mwee.cn>
2020-01-16 12:29:21 +0800

    obj 深层比较

---
+++ src/untils/ImmutableCompare.js
@@ -0,0 +1,33 @@
+/**
+ * Created by lyl on 20/1/16.
+ */
+import Immutable, {Map, List} from 'immutable';
+let is = Immutable.is.bind(Immutable);
+
+export default function ImmutableCompare(objA, objB) {
+    if (objA === objB || is(objA, objB)) {
+        return true;
+    }
+
+    if (typeof objA !== 'object' || objA === null ||
+        typeof objB !== 'object' || objB === null) {
+        return false;
+    }
+
+    var keysA = Object.keys(objA);
+    var keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+        return false;
+    }
+
+    // Test for A's keys different from B.
+    var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+    for (var i = 0; i < keysA.length; i++) {
+        if (!bHasOwnProperty(keysA[i]) || !is(objA[keysA[i]], objB[keysA[i]])) {
+            return false;
+        }
+    }
+
+    return true;
+}
